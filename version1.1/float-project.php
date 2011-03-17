9c860384c0d6ba2b172083e2d835258fc480e4b7
Gaurav <gaurav@StalkNinja.com>
2011-03-17 10:07:31 +0530

    project_file_update

--- version1.1/float-project.php
+++ version1.1/float-project.php
@@ -1,103 +1,133 @@
 <?php
-	
-	$error = false;
-	$message = "";
-	$data = array();
-			
-	include("mysql.php");
-	include ("smarty.php");
-	
-	$dir = "files/".$_GET['id'];
 
-	//check if project is not open anymore
-	if ( !isset($_GET['id']) or ( $data = mysql_fetch_assoc(mysql_query("SELECT * FROM Project WHERE Id=".$_GET['id'])) and $data['Status'] and $data['Status'] != "open" ) ){
-		$smarty->assign("error",true);
-		$smarty->assign("message","This project editing is not allowed");
-		$smarty->assign("tpl_name","error.tpl");
-		$smarty->display("main.tpl");
-		exit;
-	}
+$error = false;
+$message = "";
+$data = array();
 
-	if (count($_POST) > 0) { //save the data
-	
-		$message=var_export($_POST, TRUE);
-		$to="admin@stalkninja.com";
-		$subject="Project udpadated ";
-		$header="From: admin@stalkninja.com\r\n";
-		mail($to, $subject, $message);
+include("mysql.php");
+include ("smarty.php");
 
-		
-		//put upload data
-		if (!is_dir($dir)) {
-			mkdir($dir);
-		}
-		
-		//delete all marked files
-		foreach ($_POST as $key=>$value) {
-			if (preg_match("/delete-(.*)/",$key,$match)) {
-				//echo $key; echo $value;
-				if ($value == "on") {
-					$file = preg_replace("/_(\w*)$/",".$1",$match[1]); 
-					//echo $file;
-					@unlink($dir."/".$file);
-				}
-				unset($_POST[$key]);
-			}
-		}
+if (!isset($_GET['token']) || $_GET['token'] == NULL) {
+    $smarty->assign("error", true);
+    $smarty->assign("message", "This project editing is not allowed");
+    $smarty->assign("tpl_name", "error.tpl");
+    $smarty->display("main.tpl");
+    exit;
+}
+$token=mysql_escape_string($_GET['token']);
 
-		//upload new files
-		foreach ($_FILES as $name=>$file){
-				//var_dump($name); var_dump($file);
-				move_uploaded_file($file["tmp_name"],$dir."/".$file["name"]);
-				unset($_POST[$name]);
-		}
+$query="select count(*) as count from project where token='".$token."'";
+$data1=mysql_fetch_assoc(mysql_query($query));
 
-		
-		$update_param = array();
-		foreach ($_POST as $key=>$value) {
-			if ($key != "Contact") {
-				if ($value) {
-					$update_param[] = "$key='".mysql_real_escape_string($value)."'";
-				}
-				else {
-					$update_param[] = "$key = NULL";
-				}
-			}
-		}
-		
+
+if ($data1['count']!=1) {
+    $smarty->assign("error", true);
+    $smarty->assign("message", "This project editing is not allowed");
+    $smarty->assign("tpl_name", "error.tpl");
+    $smarty->display("main.tpl");
+    exit;
+}
+
+
+
+
+$query = "select id from project where token='" . mysql_real_escape_string($_GET['token']) . "'";
+$data2 = mysql_fetch_assoc(mysql_query($query));
+$id = $data2['id'];
+
+
+
+$dir = "files/" . $id;
+
+//check if project is not open anymore
+if (( $data = mysql_fetch_assoc(mysql_query("SELECT * FROM Project WHERE Id=$id")) and $data['Status'] and $data['Status'] != "open" )) {
+    $smarty->assign("error", true);
+    $smarty->assign("message", "This project editing is not allowed");
+    $smarty->assign("tpl_name", "error.tpl");
+    $smarty->display("main.tpl");
+    exit;
+}
+
+
+if (count($_POST) > 0) { //save the data
+    $message = var_export($_POST, TRUE);
+    $to = "admin@stalkninja.com";
+    $subject = "Project udpadated ";
+    $header = "From: admin@stalkninja.com\r\n";
+   mail($to, $subject, $message);
+
+
+    //put upload data
+    if (!is_dir($dir)) {
+        mkdir($dir);
+    }
+
+    //delete all marked files
+    foreach ($_POST as $key => $value) {
+        if (preg_match("/delete-(.*)/", $key, $match)) {
+            //echo $key; echo $value;
+            if ($value == "on") {
+                $file = preg_replace("/_(\w*)$/", ".$1", $match[1]);
+                //echo $file;
+                @unlink($dir . "/" . $file);
+            }
+            unset($_POST[$key]);
+        }
+    }
+
+    //upload new files
+    foreach ($_FILES as $name => $file) {
+        //var_dump($name); var_dump($file);
+        move_uploaded_file($file["tmp_name"], $dir . "/" . $file["name"]);
+        unset($_POST[$name]);
+    }
+
+
+    $update_param = array();
+    foreach ($_POST as $key => $value) {
+        if ($key != "Contact") {
+            if ($value) {
+                $update_param[] = "$key='" . mysql_real_escape_string($value) . "'";
+            } else {
+                $update_param[] = "$key = NULL";
+            }
+        }
+    }
+    $update_param[]="Status='Open'";
+    $update_param[]="DateCreated='".  date("Y-m-d")."'";
+
 //		var_dump($update_param);
-		
-		$query = "UPDATE Project SET ".join(",",$update_param)." WHERE Id=".$_GET['id'];
-		//var_dump($query);
-		if (mysql_query($query)) {
-				$message = "The project is submitted. It would be reviewed by Ashish/Gaurav & either of them will get back in touch with you with the status within 24 hours. You are free to update the project content anytime you like to, before it goes live.";
-		}
-		else {
-			$error = true;
-			$message =  mysql_error();
-		}
 
-		$data = $_POST;
-	}
-	
-		//read all uploaded files & show them for deleting
-		$files = array();
-		if (is_dir($dir) and $handle = opendir($dir)) {
+    $query = "UPDATE Project SET " . join(",", $update_param) . " WHERE Id=$id";
+    //var_dump($query);
+    if (mysql_query($query)) {
+        $message = "The project is submitted. You are free to update the project content anytime you like to. Make sure that you subscribe to Disqus board and keep cheking for comments by Ninja's.";
+    } else {
+        $error = true;
+        $message = mysql_error();
+    }
 
-			/* This is the correct way to loop over the directory. */
-			while (false !== ($file = readdir($handle))) {
-				if ($file != "." and $file != "..") {
-					$files[] = $file;
-				}
-			}
-			closedir($handle);
-		}
-		$smarty->assign("files",$files);
+    $data = $_POST;
+}
+
+//read all uploaded files & show them for deleting
+$files = array();
+if (is_dir($dir) and $handle = opendir($dir)) {
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." and $file != "..") {
+            $files[] = $file;
+        }
+    }
+    closedir($handle);
+}
+$smarty->assign("files", $files);
 
 
-	$smarty->assign("data",$data);
-	$smarty->assign("error",$error);
-	$smarty->assign("message",$message);
-	$smarty->assign("tpl_name","float-project.tpl");
-	$smarty->display("main.tpl");
+$smarty->assign("data", $data);
+$smarty->assign("error", $error);
+$smarty->assign("message", $message);
+$smarty->assign("tpl_name", "float-project.tpl");
+$smarty->display("main.tpl");
 ?>
