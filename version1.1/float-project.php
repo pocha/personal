afd5132d50bb6b73f70cc730dbad4172ac1cda90
Gaurav <gaurav@StalkNinja.com>
2011-03-24 00:52:14 +0530

    mail_comment_removed

--- version1.1/float-project.php
+++ version1.1/float-project.php
@@ -99,7 +99,7 @@ if ($_GET['token'] == NULL) {
         $header = "From: admin@stalkninja.com\r\n";
         $message = "Name: $name \n Email: $email \n Thanks for submitting Project $title at StalkNinja. Use the link
                 below to edit project whenever you need. \n http://stalkninja.com/float-project.php?token=$token";
-        //mail($to, $subject, $message);
+        mail($to, $subject, $message);
         //put upload data
         if (!is_dir($dir)) {
             mkdir($dir);
@@ -213,7 +213,7 @@ else {
         $to = "admin@stalkninja.com";
         $subject = "Project updated ";
         $header = "From: admin@stalkninja.com\r\n";
-        //mail($to, $subject, $message);
+        mail($to, $subject, $message);
         
         //put upload data
         if (!is_dir($dir)) {
