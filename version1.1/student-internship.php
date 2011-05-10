23b63b3b7076157d89aa5332052f1f4b225eaa94
pocha <pocha.sharma@gmail.com>
2011-05-10 12:13:04 +0000

    updated the height of the student internship box

--- version1.1/student-internship.php
+++ version1.1/student-internship.php
@@ -1,7 +1,7 @@
 <?php
 	include("smarty.php");
 	$smarty->assign("heading","");
-	$smarty->assign("height","1500");
+	$smarty->assign("height","1600");
 	$smarty->assign("form","https://spreadsheets.google.com/embeddedform?formkey=dDE4dm5RYlhqa1E2d0tQN2RTWnNaTmc6MQ");
 	$smarty->assign("tpl_name","google-form.tpl");
 	$smarty->display("main.tpl");
