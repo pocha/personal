55e961e2494cefdaf3a116afc3b12691758e56a4
pocha <pocha.sharma@gmail.com>
2011-08-11 03:03:35 +0530

    put pending approval in index.php

--- version1.1/index.php
+++ version1.1/index.php
@@ -4,7 +4,7 @@
 	include("mysql.php");
 
 	$projects = array();
-	$result = mysql_query("SELECT * FROM Project WHERE Status != 'cancelled' ORDER BY DateCreated DESC");
+	$result = mysql_query("SELECT * FROM Project WHERE Status != 'cancelled' AND Status != 'pending-approval' ORDER BY DateCreated DESC");
 	while ($line = mysql_fetch_assoc($result)){
 		$data = mysql_fetch_assoc(mysql_query("SELECT * FROM Startup WHERE Id=".$line['StartupId'].""));
 		$line['StartupName'] = $data['Name'];
